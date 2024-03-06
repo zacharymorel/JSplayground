11cc1999f1588d47df51026089486e0574cca9a7
zacharymorel <zacharyjmorel@gmail.com>
2024-03-06 13:45:19 -0500

    First off by one with inserts and removes

--- dataStructures/linkedList.ts
+++ dataStructures/linkedList.ts
@@ -27,12 +27,12 @@
 // Example of using Generics ofc
 interface ILinkedList<T> {
 	get length(): number;
-	get(index: number): T | undefined;
-	insertAt(node: T, index: number): void;
+	get(index: number): T | null;
+	insertAt(node: T, index: number): void | null;
 	prepend(node: T): void;
 	append(node: T): void;
 	remove(node: ListNode<T>): void;
-	removeAt(index: number): T | undefined;
+	removeAt(index: number): void | null;
 	print(): void;
 }
 
@@ -56,6 +56,7 @@ class ListNode<T> implements IListNode<T> {
 
 class LinkedList<T> implements ILinkedList<T> {
 	private _headN: ListNode<T> | null = null;
+
 	private _traverse(): T[] {
 		const arr: T[] = [];
 
@@ -68,12 +69,16 @@ class LinkedList<T> implements ILinkedList<T> {
 
 		return addToArr(this._headN);
 	}
+
 	public get length(): number {
 		return this._traverse().length;
 	}
-	public get(index: number): T | undefined {
-		throw new Error('Method not implemented.');
+
+	public get(index: number): T | null {
+		if (!index) return null;
+		return this._traverse()?.[index];
 	}
+
 	public append(node: T): void {
 		const n = new ListNode(node);
 
@@ -82,12 +87,8 @@ class LinkedList<T> implements ILinkedList<T> {
 		} else {
 			// Recursive function to iterate through the structure and find the last node.
 			const getLast = (currNode: ListNode<T>): ListNode<T> => {
-				// if the node has a next
-				if (currNode?.next) {
-					return getLast(currNode.next);
-				} else {
-					return currNode;
-				}
+				// if the node has a next, continue recursive function, ELSE return value
+				return currNode?.next ? getLast(currNode.next) : currNode;
 			};
 
 			const lastN = getLast(this._headN);
@@ -100,9 +101,31 @@ class LinkedList<T> implements ILinkedList<T> {
 			}
 		}
 	}
-	public insertAt(node: T, index: number): void {
-		throw new Error('Method not implemented.');
+
+	public insertAt(node: T, index: number): void | null {
+		const len = this.length;
+		if (!index || len === 0 || len - 1 < index) return null;
+
+		const n = new ListNode(node);
+		let currI = 0;
+		let currN = this._headN;
+		let prevN;
+
+		// While loop to walk up the graph to the index just before where we want to insert at.
+		while (currI < index) {
+			currN = currN ? currN.next : null;
+			prevN = currN ? currN.prev : null;
+			currI++;
+		}
+
+		if (n) {
+			n.prev = prevN ? prevN : null;
+			n.next = currN ? currN : null;
+			if (prevN) prevN.next = n;
+			if (currN) currN.prev = n;
+		}
 	}
+
 	public prepend(node: T): void {
 		const n = new ListNode(node);
 		if (!this._headN) this._headN = n;
@@ -115,19 +138,48 @@ class LinkedList<T> implements ILinkedList<T> {
 			this._headN = n;
 		}
 	}
+
 	// Removes node from graph
 	public remove(node: ListNode<T>): void {
 		// Case where the node to remove is the first node.
 		if (!this._headN?.prev) this._headN = node.next;
 		else {
 			// Get node to delete's previous node and assign it's next value the node to delete's next.
-			const nodeToDeletePrev = node.prev;
-			if (nodeToDeletePrev?.next) nodeToDeletePrev.next = node.next;
+			const deleteNodePrev = node.prev;
+			const deleteNodeNext = node.next;
+			if (deleteNodePrev?.next) deleteNodePrev.next = deleteNodeNext;
+			if (deleteNodeNext?.prev) deleteNodeNext.prev = deleteNodePrev;
 		}
 	}
-	public removeAt(index: number): T | undefined {
-		throw new Error('Method not implemented.');
+
+	// Removes node from graph at index
+	public removeAt(index: number): void | null {
+		const len = this.length;
+		if (len === 0 || !index) return null;
+
+		let currI = 0;
+		let currN = this._headN;
+		let prevN;
+
+		// Walk up the graph to find node to remove.
+		while (currI < index) {
+			// Assign the current to the next node
+			currN = currN?.next ? currN.next : null;
+			// Assign the prev
+			prevN = currN?.prev ? currN.prev : null;
+			currI++;
+		}
+
+		// Remove at index
+		if (currN) {
+			const nodeAfterCurr = currN.next;
+			if (prevN) prevN.next = nodeAfterCurr;
+			if (currN.next) {
+				if (nodeAfterCurr && prevN) nodeAfterCurr.prev = prevN;
+			}
+		}
 	}
+
 	public print(): void {
 		console.log('LinkedList as an Array: ', this._traverse());
 	}
@@ -147,9 +199,32 @@ const post4 = new Post('Post 4');
 const post5 = new Post('Post 5');
 
 const postList = new LinkedList<Post>();
+
+console.log('PREPEND & APPEND');
+// APPEND
 postList.append(post1);
 postList.append(post2);
 postList.append(post3);
+
+// PREPEND
 postList.prepend(post4);
 postList.prepend(post5);
 postList.print();
+console.log('');
+
+// INSERT AT
+console.log('INSERT');
+postList.insertAt(new Post('Post 6'), 2);
+postList.print();
+console.log('');
+
+// REMOVE AT
+console.log('REMOVE');
+postList.removeAt(2);
+postList.print();
+console.log('');
+
+// GET AT
+console.log('GET at index 3');
+console.log(postList.get(3));
+console.log('');
